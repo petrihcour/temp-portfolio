0eb183034be44cd51fcce89bff8af8cf6b652c37
Vanessa <zdperfectx@gmail.com>
2024-01-09 12:31:59 -0600

    adj image margin on hero photo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -84,7 +84,7 @@ const Hero = () => {
         {/* Your image comes here. Feel free to remove image if you don't plan to have one.*/}
         <div>
           <img
-            className="absolute bottom-0 ml-[55vw] 
+            className="absolute bottom-0 ml-[50vw] 
             lg:ml-[67vw] md:ml-[60vw] xmd:ml-[60vw] 2xl:ml-[72vw]
             sm:h-[60vh] md:h-[65vh] xl:h-[80vh]"
             src={vanessa}

fe7dc853b6c45a43554217c5a5a73dea94f63804
Vanessa <zdperfectx@gmail.com>
2024-01-09 12:59:32 -0600

    added resume

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -102,7 +102,7 @@ const Experience = () => {
               ease-in-out"
               onClick={() =>
                 window.open(
-                  'resume link', //paste the link to your resume here
+                  'https://docs.google.com/document/d/1kN7wQIO85j9Pzz4yKQepAgWuNebZxQRyOYne5_OJixE/edit?usp=sharing', //paste the link to your resume here
                   '_blank'
                 )
               }
